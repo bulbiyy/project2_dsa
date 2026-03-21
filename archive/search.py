681f54df6c58523f9ebd35f3b4daa0f94ecaec5b
alina.vasina <alina.vasina@ufl.edu>
2026-03-20 22:08:04 -0400

    search any amount of values (as a list)

--- archive/search.py
+++ archive/search.py
@@ -1,19 +1,22 @@
 import pandas as pd
 
 
-def search_data(search_word):
+def search_data(search_words):
     df = pd.read_csv("fra_cleaned.csv", encoding="latin-1", sep=";")
     df.columns = df.columns.str.strip()
 
     cols = ["Top", "Middle", "Base"]
 
-    mask = df[cols].apply(
-        lambda col: col.str.lower().str.contains(search_word.lower(), na=False)
-    ).any(axis=1)
+    words = [w.lower() for w in search_words]
 
+    mask = df[cols].apply(
+        lambda col: col.str.lower().apply(
+            lambda cell: any(word in cell for word in search_words) if isinstance(cell, str) else False
+        )
+    ).any(axis=1)
     return df[mask]
 
 
 #check
-matches = search_data("blood orange")
+matches = search_data(["blood orange", "vanilla"])
 print(matches)
