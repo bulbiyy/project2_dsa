0585f931ed1499ea26e26b8cdd1c8fd84e683e45
alina.vasina <alina.vasina@ufl.edu>
2026-03-20 23:06:21 -0400

    correct data set

--- archive/search.py
+++ archive/search.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# -------- Search in clean database -----------
 
 def search_data(search_words):
     df = pd.read_csv("fra_cleaned.csv", encoding="latin-1", sep=";")
@@ -7,7 +8,7 @@ def search_data(search_words):
 
     cols = ["Top", "Middle", "Base"]
 
-    words = [w.lower() for w in search_words]
+    search_words = [w.lower() for w in search_words]
 
     mask = df[cols].apply(
         lambda col: col.str.lower().apply(
@@ -17,6 +18,33 @@ def search_data(search_words):
     return df[mask]
 
 
-#check
-matches = search_data(["blood orange", "vanilla"])
-print(matches)
+# ----------- Check ----------------
+# matches = search_data(["blood orange", "vanilla"])
+# print(matches)
+
+# ---------- Use second database for description ----------
+def merge_data(search_words):
+    filtered_df = search_data(search_words)
+
+    df2 = pd.read_csv("fra_perfumes.csv", encoding="latin-1", sep=",")
+    df2.columns = df2.columns.str.strip()
+
+    # Check urls
+    filtered_df["url"] = filtered_df["url"].str.lower().str.strip()
+    df2["url"] = df2["url"].str.lower().str.strip()
+
+    key = "url"
+    print(len(set(filtered_df["url"]) & set(df2["url"])))
+    cols_to_add = ["Top", "Middle", "Base"]
+
+    result = pd.merge(
+        df2,
+        filtered_df[[key] + cols_to_add],
+        on=key,
+        how="inner"
+    )
+
+    return result
+
+# ---------- Check final data base -----------------
+print(merge_data(["aldehydes", "vanilla"]))
